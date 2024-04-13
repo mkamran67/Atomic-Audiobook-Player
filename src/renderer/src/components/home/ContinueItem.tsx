98dab0ec2e524c0abd887ebb0d9098d6e809177a
Muhammad Kamran <mkamran1967@gmail.com>
2024-04-12 21:34:03 -0700

    Home page

---
+++ src/renderer/src/components/home/ContinueItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+function ContinueItem({ }: Props) {
+  return (
+    <div>ContinueItem</div>
+  );
+}
+
+export default ContinueItem;
--- src/renderer/src/components/pages/Home.tsx
+++ src/renderer/src/components/pages/Home.tsx
@@ -1,26 +1,18 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import TimeLine from "../home/TimeLine";
 
 type Props = {};
 
 export default function Home({ }: Props) {
   return (
-    <div className="w-full h-full">
-      <span className="inline-flex rounded-md shadow-sm isolate">
-        <button
-          type="button"
-          className="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-        >
-          <span className="sr-only">Previous</span>
-          <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
-        </button>
-        <button
-          type="button"
-          className="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-        >
-          <span className="sr-only">Next</span>
-          <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
-        </button>
-      </span>
+    <div className="flex w-full h-full">
+      <section className="w-3/4 h-full bg-gray-800">
+        <h3 className="p-2 text-xl text-ellipsis">
+          Continue Reading
+        </h3>
+      </section>
+      <section className="m-2">
+        <TimeLine />
+      </section>
     </div>
   );
 }
