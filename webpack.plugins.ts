78eec7ea1d246b78ad7821b2411dfe66d9094b06
MK <mkamran1967@gmail.com>
2026-02-23 19:10:26 -0800

    theme updated

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,11 +1,32 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { chmodSync } from 'fs';
+import { join } from 'path';
+import type { Compiler } from 'webpack';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
+class FixNativeBinaryPermissionsPlugin {
+  apply(compiler: Compiler) {
+    compiler.hooks.afterEmit.tap('FixNativeBinaryPermissionsPlugin', (compilation) => {
+      for (const assetName of Object.keys(compilation.assets)) {
+        if (assetName.includes('native_modules/ffprobe')) {
+          const outputPath = join(compiler.outputPath, assetName);
+          try {
+            chmodSync(outputPath, 0o755);
+          } catch {
+            // Ignore if file doesn't exist yet
+          }
+        }
+      }
+    });
+  }
+}
+
 export const plugins = [
   new ForkTsCheckerWebpackPlugin({
     logger: 'webpack-infrastructure',
-  })
+  }),
+  new FixNativeBinaryPermissionsPlugin(),
 ];
 
